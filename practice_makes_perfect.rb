68515e2da980d554b4769bdad95f139583729a73
Raphael Adams <raphael.adams@shopify.com>
2020-10-31 14:08:59 -0400

    First commit, created simulate_studying method

---
+++ practice_makes_perfect.rb
@@ -0,0 +1,18 @@
+# Practice Makes Perfect
+# Raphael Adams
+
+def simulate_studying(subject)
+  effectiveness = 0
+  study_session_num = 0
+
+  until effectiveness >= 10 do
+
+    study_session_num += 1
+    puts "Studying #{ subject }, session #{ study_session_num }!"
+    effectiveness += yield study_session_num
+    puts "Effectiveness so far: #{ effectiveness }"
+
+  end
+
+  puts "Ready for the #{ subject } exam, took #{  study_session_num } sessions."
+end
